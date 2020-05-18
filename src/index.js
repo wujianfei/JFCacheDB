0bba39642a6c5923daa549a9ceb5d8a9586b3620
feige_hunter <feige_hunter@163.com>
2020-05-18 13:16:42 +0800

    asdfasdf

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@
     const timezoneOffset = new Date().getTimezoneOffset();
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     const touchSupport = 'ontouchstart' in window;
-
+  
     const canvas = (() => {
       try {
         const canvas = document.createElement('canvas');
@@ -59,14 +59,14 @@
           4,
           17
         );
-
+  
         const result = canvas.toDataURL();
         return result;
       } catch (error) {
         return error;
       }
     })();
-
+  
     const data = JSON.stringify({
       userAgent,
       language,
@@ -83,15 +83,15 @@
       touchSupport,
       canvas,
     });
-
+  
     const murmurhash3_32_gc = (key) => {
       const remainder = key.length & 3; // key.length % 4
       const bytes = key.length - remainder;
       const c1 = 0xcc9e2d51;
       const c2 = 0x1b873593;
-
+  
       let h1, h1b, k1;
-
+  
       for (let i = 0; i < bytes; i++) {
         k1 =
           (key.charCodeAt(i) & 0xff) |
@@ -99,7 +99,7 @@
           ((key.charCodeAt(++i) & 0xff) << 16) |
           ((key.charCodeAt(++i) & 0xff) << 24);
         ++i;
-
+  
         k1 =
           ((k1 & 0xffff) * c1 + ((((k1 >>> 16) * c1) & 0xffff) << 16)) &
           0xffffffff;
@@ -107,20 +107,18 @@
         k1 =
           ((k1 & 0xffff) * c2 + ((((k1 >>> 16) * c2) & 0xffff) << 16)) &
           0xffffffff;
-
+  
         h1 ^= k1;
         h1 = (h1 << 13) | (h1 >>> 19);
         h1b =
-          ((h1 & 0xffff) * 5 + ((((h1 >>> 16) * 5) & 0xffff) << 16)) &
-          0xffffffff;
-        h1 =
-          (h1b & 0xffff) + 0x6b64 + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16);
+          ((h1 & 0xffff) * 5 + ((((h1 >>> 16) * 5) & 0xffff) << 16)) & 0xffffffff;
+        h1 = (h1b & 0xffff) + 0x6b64 + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16);
       }
-
+  
       const i = bytes - 1;
-
+  
       k1 = 0;
-
+  
       switch (remainder) {
         case 3: {
           k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
@@ -135,18 +133,16 @@
           break;
         }
       }
-
+  
       k1 =
-        ((k1 & 0xffff) * c1 + ((((k1 >>> 16) * c1) & 0xffff) << 16)) &
-        0xffffffff;
+        ((k1 & 0xffff) * c1 + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
       k1 = (k1 << 15) | (k1 >>> 17);
       k1 =
-        ((k1 & 0xffff) * c2 + ((((k1 >>> 16) * c2) & 0xffff) << 16)) &
-        0xffffffff;
+        ((k1 & 0xffff) * c2 + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
       h1 ^= k1;
-
+  
       h1 ^= key.length;
-
+  
       h1 ^= h1 >>> 16;
       h1 =
         ((h1 & 0xffff) * 0x85ebca6b +
@@ -158,235 +154,179 @@
           ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16)) &
         0xffffffff;
       h1 ^= h1 >>> 16;
-
+  
       return h1 >>> 0;
     };
-
+  
     const result = murmurhash3_32_gc(data);
     return result;
   }
-  /**
-   * 默认数据模板
-   * @param {*} options
-   */
-  function getDefaultTemlate(options) {
-    return JSON.stringify({
-      description: options.description || '', // 表描述
-      data: {}, //{id: 数据}
-      idSortList: [], // [id]
-    });
-  }
-  /**
-   *
-   * @param {*} devCallback
-   * @param {*} proCallback
-   */
-  function getDevCallback(devCallback = () => {}, proCallback = () => {}) {
-    if (process.env.NODE_ENV === 'production') {
-      return proCallback();
-    } else if (process.env.NODE_ENV === 'development') {
-      return devCallback();
-    }
-  }
-  /**
-   * 加密解密
-   * @param {*} str 字符串值
-   * @param {*} isDec false表示加密 true表示解密
-   */
-  function encryptionData(str, isDec = false, options) {
-    return getDevCallback(
-      () => {
-        return str;
-      },
-      () => {
-        return isDec ? options.decodeData(str) : options.encodeData(str);
-      }
-    );
-  }
-  /**
-   * 加密表名
-   * @param {*} tableName 表名
-   */
-  function encryptionName(tableName, options) {
-    return getDevCallback(
-      () => {
-        return tableName;
-      },
-      () => {
-        return options.md5Encode(tableName);
-      }
-    );
-  }
-
-  function getCacheType(type) {
-    return {
-      1: localStorage,
-      2: sessionStorage,
-    }[type];
-  }
-
-  /**
-   * 初始化cacheData
-   * 读取本地数据
-   */
-  let getInitCacheData = function (cacheObj, tableName, options) {
-    let cacheDataStr = '';
-    let tableValStr = cacheObj.getItem(tableName);
-    if (tableValStr) {
-      cacheDataStr = encryptionData(tableValStr, true, options);
-    } else {
-      cacheDataStr = getDefaultTemlate(options);
-    }
-    return JSON.parse(cacheDataStr);
-  };
-  /**
-   * 将表名转成md5并存储表名
-   * @param {*} cacheObj
-   * @param {*} table
-   * @param {*} cacheData
-   * @param {*} tableName
-   */
-  function setNameByMd5(cacheObj, table, cacheData, tableName, options) {
-    cacheData.data[tableName] = encryptionName(tableName, options);
-    cacheObj.setItem(
-      table.tableName,
-      encryptionData(JSON.stringify(cacheData), false, options)
-    );
-    return cacheData.data[tableName];
-  }
-  /**
-   * 构建存储表名的表
-   * @param {*} tableName
-   */
-  function getNameTable(tableName, options) {
-    const table = {
-      tableName: encryptionName(fingerprint(), options),
-      description: `存储表名的表\n其他表的表名将直接是加密的`,
-      cacheType: 1,
+  export function JFcacheDB() {
+    const that = this;
+    const constString = {
+      tbtDesc: `存储表名的表,其他表的表名将直接是加密的`,
     };
-    let cacheObj = getCacheType(table.cacheType);
-    let tableValStr = cacheObj.getItem(table.tableName);
-    let cacheDataStr = '';
-    if (tableValStr) {
-      cacheDataStr = encryptionData(tableValStr, true, options); //解密
-      let cacheData = JSON.parse(cacheDataStr);
-      if (cacheData.data[tableName]) {
-        return cacheData.data[tableName];
-      } else {
-        return setNameByMd5(cacheObj, table, cacheData, tableName, options);
-      }
-    } else {
-      cacheDataStr = getDefaultTemlate({
-        description: table.description,
+    let encryptFun = {
+      decodeData: (d) => d,
+      encodeData: (d) => d,
+      encodeTableName: (d) => d,
+    };
+    /**
+     * 获得默认模板
+     * @param {*} param0
+     */
+    function getDataTemplate({ description }) {
+      return JSON.stringify({
+        description: description || '', // 表描述
+        data: {}, //{id: 数据}
+        idSortList: [], // [id]
       });
     }
-    let cacheData = JSON.parse(cacheDataStr);
-    return setNameByMd5(cacheObj, table, cacheData, tableName, options);
-  }
-
-  /**
-   * 缓存数据
-   * @param {*} tableName_ 表名
-   * @param {*} options
-   * @param {*} cacheType
-   */
-  function BaseDB(
-    table = { description: '', tableName: '', cacheType: 1 },
-    options
-  ) {
-    let tableName = getNameTable(table.tableName, options);
-    let cacheObj = getCacheType(table.cacheType);
-    let cacheData = getInitCacheData(cacheObj, tableName, {
-      ...options,
-      description: table.description,
-    }); // 初始化
-
     /**
-     * 校验
+     * 设置加密算法
      */
-    let illegalError = function () {
-      // if (tableName.indexOf('@table') !== 0) {
-      //   throw new Error('The operation cannot be operated directly here');
-      // }
+    that.setEncryption = function ({ decodeData, encodeData, encodeTableName }) {
+      encryptFun = { decodeData, encodeData, encodeTableName };
     };
     /**
-     * 排序id
-     * @param {*} id
+     * 初始化
      */
-    let clearSortId = function (id) {
-      let sort = cacheData.idSortList;
-      for (let i = sort.length - 1; i >= 0; i--) {
-        if (sort[i] === id) {
-          cacheData.idSortList.splice(i, 1);
-        }
+    function initCacheDb(storage, tableName, description) {
+      let tableValStr = storage.getItem(tableName);
+      if (tableValStr) {
+        return JSON.parse(encryptFun.decodeData(tableValStr));
+      } else {
+        return JSON.parse(getDataTemplate({ description }));
       }
-    };
+    }
+    /**
+     * 获取存储类型
+     * @param {*} type
+     */
+    function getStorage(type) {
+      return {
+        1: localStorage,
+        2: sessionStorage,
+      }[type];
+    }
     /**
      * 事务提交
+     * @param {*} storage
+     * @param {*} key
+     * @param {*} value
      */
-    let commit = function () {
-      // 加密保存
-      cacheObj.setItem(
-        tableName,
-        encryptionData(JSON.stringify(cacheData), false, options)
-      );
-    };
+    function commit(storage, key, value) {
+      storage.setItem(key, encryptFun.encodeData(JSON.stringify(value)));
+    }
     /**
-     * 保存数据
-     * 如果已经有，则会直接覆盖
-     * @params {*} id 记录编号
-     * @params {*} data 记录值
+     * 保存表名
      */
-    this.saveOrUpdate = function (id, data = {}) {
-      if (!id) {
-        return;
+    function saveTableName({ storage, table, tableName, cacheData }) {
+      cacheData.data[tableName] = encryptFun.encodeTableName(tableName);
+      commit(storage, table.tableName, cacheData);
+      return cacheData.data[tableName];
+    }
+    /**
+     * 获得加密表名
+     */
+    function getEncryptionName(tableName) {
+      const table = {
+        tableName: encryptFun.encodeTableName(fingerprint()),
+        description: constString.tbtDesc,
+        cacheType: 1,
+      };
+      const storage = getStorage(table.cacheType);
+      let tbtStr = storage.getItem(table.tableName);
+      if (tbtStr) {
+        const tbt = JSON.parse(encryptFun.decodeData(tbtStr));
+        return tbt.data[tableName]
+          ? tbt.data[tableName]
+          : saveTableName({ storage, table, tableName, cacheData: tbt });
+      } else {
+        const tbt = JSON.parse(getDataTemplate({
+          description: table.description,
+        }));
+        return saveTableName({ storage, table, tableName, cacheData: tbt });
       }
-      illegalError();
-      cacheData.data[id] = data;
-      let sort = cacheData.idSortList;
-      for (let i = sort.length - 1; i >= 0; i--) {
-        if (sort[i] === id) {
-          cacheData.idSortList.splice(i, 1);
+    }
+    that.getTable = function({ tableName, description, cacheType }) {
+      return new Table({ tableName, description, cacheType })
+    }
+    /**
+     * 获得一张表
+     */
+    const Table = function ({ tableName, description, cacheType }) {
+      const storage = getStorage(cacheType);
+      const cacheData = initCacheDb(storage, tableName, description);
+      const encryptionName = getEncryptionName(tableName);
+      /**
+       * 排序id
+       * @param {*} id
+       */
+      function clearSortId(id) {
+        let sort = cacheData.idSortList;
+        for (let i = sort.length - 1; i >= 0; i--) {
+          if (sort[i] === id) {
+            cacheData.idSortList.splice(i, 1);
+          }
         }
       }
-      clearSortId(id);
-      cacheData.idSortList.unshift(id);
-      commit(); // 提交事务
-      return true;
-    };
-
-    // 删除某一项
-    this.removeById = function (id) {
-      illegalError();
-      delete cacheData.data[id];
-      clearSortId(id);
-      commit(); // 事务提交
-    };
-    /**
-     * 根据id取记录
-     * @params {*} id 记录编号
-     * @params {*} defaultVal 默认值
-     */
-    this.get = function (id, defaultVal) {
-      if (cacheData.data[id] !== undefined) {
-        return cacheData.data[id];
-      }
-      if (defaultVal !== undefined) {
-        return defaultVal;
-      }
-      return null;
-    };
-    // 整张表的数据
-    this.getCacheData = function () {
-      return cacheData;
-    };
-    /**
-     * 删除当前表
-     */
-    this.dropCurrentTable = function () {
-      cacheObj.removeItem(tableName);
+      /**
+       * 保存数据
+       * 如果已经有，则会直接覆盖
+       * @params {*} id 记录编号
+       * @params {*} data 记录值
+       */
+      this.saveOrUpdate = function (id, data = {}) {
+        if (!id) {
+          return;
+        }
+        cacheData.data[id] = data;
+        let sort = cacheData.idSortList;
+        for (let i = sort.length - 1; i >= 0; i--) {
+          if (sort[i] === id) {
+            cacheData.idSortList.splice(i, 1);
+          }
+        }
+        clearSortId(id);
+        cacheData.idSortList.unshift(id);
+        commit(storage, encryptionName, cacheData); // 提交事务
+        return true;
+      };
+  
+      // 删除某一项
+      this.removeById = function (id) {
+        delete cacheData.data[id];
+        clearSortId(id);
+        commit(storage, encryptionName, cacheData); // 事务提交
+      };
+      /**
+       * 根据id取记录
+       * @params {*} id 记录编号
+       * @params {*} defaultVal 默认值
+       */
+      this.get = function (id, defaultVal) {
+        if (cacheData.data[id] !== undefined) {
+          return cacheData.data[id];
+        }
+        if (defaultVal !== undefined) {
+          return defaultVal;
+        }
+        return null;
+      };
+      // 整张表的数据
+      this.getCacheData = function () {
+        return cacheData;
+      };
+      /**
+       * 删除当前表
+       */
+      this.dropCurrentTable = function () {
+        storage.removeItem(encryptionName);
+      };
     };
   }
+  
 
-  return BaseDB;
+  return JFcacheDB;
 });
